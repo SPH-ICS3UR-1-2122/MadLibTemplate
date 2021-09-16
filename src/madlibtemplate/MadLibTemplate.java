d8a8f53e4d082acb360d644ce9250570e5f56da8
Kris Zebarth <kris.zebarth@ocsb.ca>
2021-09-16 08:44:45 -0400

    Initial commit

---
+++ src/madlibtemplate/MadLibTemplate.java
@@ -0,0 +1,31 @@
+/*
+ * For this assignment, you will be creating your own MadLibs program
+ * Ask the user for AT LEAST 10 inputs. Then add them in to a silly story.
+ * You can create the story yourself or you can find one online. 
+ * Make sure your variables are all named appropriately. 
+ */
+package madlibtemplate;
+import java.util.Scanner;
+/**
+ *
+ * @author PUT YOUR NAME HERE
+ */
+public class MadLibTemplate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Create a scanner for you!
+        Scanner in = new Scanner(System.in);
+        //YOUR STUFF GOES HERE!
+        
+        
+        
+        
+        
+        //Close the scanner. This is the last line of your program
+        in.close();
+    }
+    
+}
